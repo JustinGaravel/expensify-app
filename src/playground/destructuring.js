7ab68f563c4ab9239079cb737e62629a58848e08
Justin Garavel <xxjeep@gmail.com>
2017-09-28 09:39:33 +0900

    refactor redux code using destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// OBJECT DESTRUCTURING
+
 // const person = {
 //   name: 'Justin',
 //   age: 29,
@@ -20,15 +22,27 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
 
-const {name: publisherName = 'Self-Published'} = book.publisher;
+// const {name: publisherName = 'Self-Published'} = book.publisher;
 
 
-console.log(publisherName); // set default value of 'Self-Published'
+// console.log(publisherName); // set default value of 'Self-Published'
+
+
+// ARRAY DESTRUCTURING
+
+const address = ['1299 South Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+const [, city, state = 'Connecticut'] = address;
+console.log(`You are in ${city}, ${state}.`);
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [itemName, , medPrice] = item;
+console.log(`A medium ${itemName} costs ${medPrice}.`);
