148c7730eb26056d92324818c0d81cca10d262bc
Justin Garavel <xxjeep@gmail.com>
2017-10-01 18:08:25 +0900

    reorganizing Redux and testing with app.js

---
+++ src/selectors/expenses.js
@@ -0,0 +1,21 @@
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = typeof text !== 'string' || expense.description.toLowerCase().includes(text.toLowerCase());
+
+    // figure out if text filter const is in expense.description
+    // use include functionality
+    // convert both strings to lower case
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a,b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+};
+
+export default getVisibleExpenses;
