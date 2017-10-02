fea6b850ba97778d5c715c15ee5a3d26c33c0d2d
Justin Garavel <xxjeep@gmail.com>
2017-10-02 10:34:59 +0900

    set up provider to provide store and created HOC to get things off store

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -12,11 +13,17 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('wat'));
+store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log('visibleExpenses', visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
